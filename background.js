c0a11750cbcda719cd0f9170277b504fca586f8d
soleilvi <soleil.vivero@gmail.com>
2023-09-23 08:01:56 -0700

    Extension Tutorial 3

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
 // listens for updates in tab system and checks if the most recent tab is a YouTube page
+// Calls a function addListener, which takes another function as an argument. We are using an arrow function.
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
+      const queryParameters = tab.url.split("?")[1];  // Saves the text after the question mark in a YouTube URL
+      const urlParameters = new URLSearchParams(queryParameters);  // Interface that works with URL parameters
   
+      // Communicates that a new URL is loaded up
+      // Delete: 12:32
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
         videoId: urlParameters.get("v"),
