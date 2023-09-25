eef2793016f22143e7f0e40b7157cdb92b418470
soleilvi <soleil.vivero@gmail.com>
2023-09-25 09:18:09 -0700

    Extension Tutorial 5

--- contentScript.js
+++ contentScript.js
@@ -13,18 +13,27 @@
         }
     });
 
+    const fetchBookmarks = () => {
+        return new Promise((resolve) => {
+            chrome.storage.sync.get([currentVideo], (obj) => {
+                resolve.apply(obj[currentVideo] ? JSON.parse(obj[currentVideo]): []);
+            })
+        })
+    }
+
     const newVideoLoaded = () => {
         const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];  // Gets the first element that matches "bookmark-btn"
 
         // If a bookmark button does not exist on the web page, create an image element for the bookmark buttons
         if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
+            const bookmarkBtn = document.createElement("img");  // "document" is your web page
 
             bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
             bookmarkBtn.className = "ytp-button " + "bookmark-btn";
             bookmarkBtn.title = "Click to bookmark current timestamp";
 
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            // You can find these two through the "inspect" command on your browser 
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];  // Running this command (without the variable) on your browser console returns the YouTube left controls
             youtubePlayer = document.getElementsByClassName("video-stream")[0];
             
             youtubeLeftControls.append(bookmarkBtn);
@@ -33,21 +42,23 @@
     }
 
     const addNewBookmarkEventHandler = () => {
-        const currentTime = youtubePlayer.currentTime;
+        const currentTime = youtubePlayer.currentTime;  // Variable that keeps video time in the YouTube website
         const newBookmark = {
             time: currentTime,
             desc: "Bookmark at " + getTime(currentTime),
         };
         console.log(newBookmark);
 
+        // Maps back to a set of bookmarks in chrome storage
         chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))  // The sort makes it so that the bookmarks are ordered by time
         });
     }
 
-    newVideoLoaded();
+    newVideoLoaded();  // Fix for button not showing up if you refresh your video page
 })();
 
+// Converts the seconds from currentTime into a readable format for the computer
 const getTime = t => {
     var date = new Date(0);
     date.setSeconds(1);
