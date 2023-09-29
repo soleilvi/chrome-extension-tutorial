7b3b4b7f20ef5bd3b60348b12bcac9ce8b80be17
soleilvi <soleil.vivero@gmail.com>
2023-09-29 13:45:08 -0700

    Extension Tutorial 8

--- popup.js
+++ popup.js
@@ -12,4 +12,20 @@ const onDelete = e => {};
 const setBookmarkAttributes =  () => {};
 
 // native window event that is triggerd when you load an HTML document (when we want to show bookmarks)
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+
+    // I've already explained the following 3 lines in backgorund.js
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+    const currentVideo = urlParameters.get("v");
+
+    if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]): [];
+        })
+    }
+    else {
+
+    }
+});
